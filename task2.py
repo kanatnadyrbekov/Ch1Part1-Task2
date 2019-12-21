d6b9d1abc8927a771a7026baaae57ee77fc6608d
kanat <kanatnadyrbekov@gmail.com>
2019-12-21 11:19:33 +0600

    Ch1Part1 Task2

---
+++ task2.py
@@ -0,0 +1,13 @@
+def square_number(number):
+    return number ** 2
+    #write your code here...
+    #DO NOT remove lines below here, this is designed to test your code.
+
+
+def test_square_number():
+    assert square_number(2) == 4
+    assert square_number(8) == 64
+    assert square_number(10) == 100
+    print("YOUR CODE IS CORRECT!")
+    
+test_square_number()
